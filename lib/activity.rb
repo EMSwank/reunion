cca54b042e96b60ce438f6d4c862650106939e7c
Eliot Swank <eliotswank@gmail.com>
2018-01-03 10:58:32 -0700

    Independent timed exercise

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,23 @@
 class Activity
+
+  attr_reader :type, :participant, :amount
+
+  def initialize(type, participant, amount)
+    @type = type
+    @participant = participant
+    @amount = amount
+  end
+
+  def activity_type
+    type
+  end
+
+  def person_paid
+    {@participant => @amount}
+  end
+
+  def total_cost
+    total = {amounts: @amount}
+    # total.values.inject(:+)
+  end
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,36 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def test_it_exists
+    activity = Activity.new("bowling", "Jim", 20)
+
+    assert_instance_of Activity, activity
+  end
+
+  def test_it_take_an_activity_type
+    activity = Activity.new("bowling", "Jim", 20)
+
+    assert_equal "bowling", activity.activity_type
+  end
+
+  def test_it_takes_a_person_and_an_amount
+    activity = Activity.new("bowling", "Jim", 20)
+
+    assert_equal ({"Jim"=>20}), activity.person_paid
+  end
+
+  def test_it_can_take_multiple_people
+    activity1 = Activity.new("bowling", "Jim", 20)
+    activity2 = Activity.new("eating", "Dave", 25)
+
+    assert_equal ({"Jim"=>20}), activity1.person_paid
+    assert_equal ({"Dave" => 25}), activity2.person_paid
+  end
+
+  def test_it_calculates_total_cost
+    activity1 = Activity.new("bowling", "Jim", 20)
+    activity2 = Activity.new("eating", "Dave", 25)
+
+    assert_equal 45, activity2.total_cost
+  end
 end
